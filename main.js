183da5ab47c3681d6a456fb51bd6ed8fbf5c26b8
Pierre BAUSIERE <pierre.bausiere@bell.ca>
2021-08-05 11:02:14 +0200

    <init> clean template + add board

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 import * as THREE from 'THREE';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import './style.css';
 
 // Renderer
@@ -16,19 +14,18 @@ const scene = new THREE.Scene();
 
 // Camera
 const camera = new THREE.PerspectiveCamera(74, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 camera.position.setZ(30);
-camera.position.x = -3;
 
 renderer.render(scene, camera);
 
 // Object
-const geometry = new THREE.TorusGeometry(8,3,16,100);
+const geometry = new THREE.PlaneGeometry(75, 40);
 const material = new THREE.MeshStandardMaterial({
-  color: 0xFF6347,
-  wireframe: true
+  color: 'green',
 });
-const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
+const gameboard = new THREE.Mesh(geometry, material);
+scene.add(gameboard);
 
 // Light
 const pointLight = new THREE.PointLight(0xFFFFFF);
@@ -42,82 +39,88 @@ scene.add(pointLight, ambientLight);
 // const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper);
 
-// Controls
-const controls = new OrbitControls(camera, renderer.domElement);
+function addFondation() {
+  // Object
+  const fondationGeo = new THREE.PlaneGeometry(3, 4);
+  const fondationMaterial = new THREE.MeshStandardMaterial({
+    color: 'white',
+  });
 
-
-// Moon
-const moonTexture = new THREE.TextureLoader().load('assets/moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('assets/normal.jpg');
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: moonTexture,
-    normalMap: normalTexture
-  })
-)
-scene.add(moon);
-
-moon.position.z = 30;
-moon.position.x = -10;
-
-// Stormtrooper + dat.GUI
-// const gui = new dat.GUI();
-
-// Array of animations mixers
-const mixers = [];
-
-const gltfLoader = new GLTFLoader();
-let stormtrooper = null;
-gltfLoader.load('assets/dancing_stormtrooper/scene.gltf', (trooper) => {
-  trooper.scene.position.set(7, -1, -9);
-
-  // gui.add(trooper.scene.position, 'x', -20, 20, 1);
-  // gui.add(trooper.scene.position, 'y', -20, 20, 1);
-  // gui.add(trooper.scene.position, 'z', -20, 20, 1);
-
-  const mixer = new THREE.AnimationMixer(trooper.scene);
-  mixers.push(mixer);
-  const start = mixer.clipAction(trooper.animations[0]);
-  start.play();
-
-  stormtrooper = trooper.scene;
-  scene.add(trooper.scene);
-});
-
-
-
-// Scroll animation
-function moveCamera(){
-  let t = document.body.getBoundingClientRect().top;
-  if(t>0){
-    t = -t;
+  for(let i=0; i<4; i++) {
+    const fondation = new THREE.Mesh(fondationGeo, fondationMaterial);
+    scene.add(fondation);
+    fondation.position.setX(15 + i*5);
+    fondation.position.setY(15);
   }
-  camera.position.z = t * -0.01;
-  camera.position.y = t * -0.0002;
-  camera.position.x = t * -0.0002;
-
-  if(stormtrooper){
-    stormtrooper.position.y = t * -0.02;
-  }
-  
 }
 
-document.body.onscroll = moveCamera;
-moveCamera();
+addFondation();
+
+function addReserve() {
+  // Object
+  const fondationGeo = new THREE.PlaneGeometry(3, 4);
+  const fondationMaterial = new THREE.MeshStandardMaterial({
+    color: 'white',
+  });
+
+  for(let i=0; i<2; i++) {
+    const fondation = new THREE.Mesh(fondationGeo, fondationMaterial);
+    scene.add(fondation);
+    fondation.position.setX(-30 + i*5);
+    fondation.position.setY(15);
+  }
+}
+
+addReserve();
+
+function addTableau() {
+  // Object
+  const fondationGeo = new THREE.PlaneGeometry(3, 4);
+  const fondationMaterial = new THREE.MeshStandardMaterial({
+    color: 'white',
+  });
+
+  for(let i=0; i<7; i++) {
+    const fondation = new THREE.Mesh(fondationGeo, fondationMaterial);
+    scene.add(fondation);
+    fondation.position.setX(i*5);
+    fondation.position.setY(5);
+  }
+}
+
+addTableau();
+
+const ROUGE = 'rouge';
+const NOIR = 'noir';
+
+const COEUR = { symbol: '❤️', couleur: ROUGE };
+const PIQUE = { symbol: '♠️', couleur: NOIR };
+const CARREAU = { symbol: '♦️', couleur: ROUGE };
+const TREFLE = { symbol: '♣️', couleur: NOIR };
+
+const SYMBOLS = [COEUR, PIQUE, CARREAU, TREFLE];
+
+function generateCards() {
+  const cards = [];
+  const fondationGeo = new THREE.PlaneGeometry(3, 4);
+  const fondationMaterial = new THREE.MeshStandardMaterial({
+    color: 'white',
+  });
+  SYMBOLS.forEach(symbol => {
+    for(let i=0;i<54;i++) {
+      const fondation = new THREE.Mesh(fondationGeo, fondationMaterial);
+      scene.add(fondation);
+      fondation.position.setX(i*5);
+      fondation.position.setY(5);
+    }
+  });
+  return cards;
+}
 
 //Animation
 let previousRAF = null;
 function animate(t){
   requestAnimationFrame(animate);
-
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.x += 0.01;
-
-  moon.rotation.y += 0.005;
-
-  controls.update();
 
   renderer.render(scene, camera);
   if(previousRAF === null){
@@ -125,32 +128,9 @@ function animate(t){
   }
 
   const timeElapsed = (t-previousRAF) * 0.001;
-  if(mixers){
-    mixers.map(m => {m.update(timeElapsed)})
-  }
+ 
   previousRAF = t;
 }
-
-// Add Stars
-function addStars(stars_count){
-  Array(stars_count).fill().forEach(addStar);
-}
-
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25);
-  const material = new THREE.MeshStandardMaterial({color: 0xffffff});
-  const star = new THREE.Mesh(geometry, material);
-
-  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-  
-  star.position.set(x,y,z);
-  scene.add(star);
-}
-addStars(200);
-
-// Background 
-const spaceTexture = new THREE.TextureLoader().load('assets/space.jpg');
-scene.background = spaceTexture;
 
 // Window resizing
 function onWindowResize() {
